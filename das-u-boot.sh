7ebac30ca0f28397ef55fb5256406a5b8bf0be93
Marty Jones <mj8263788@gmail.com>
2026-05-10 22:20:39 -0300

    add rockchip rk3399 Signed-off-by: Marty Jones <mj8263788@gmail.com>

--- das-u-boot.sh
+++ das-u-boot.sh
@@ -21,8 +21,8 @@ fi
 		git clone --depth 1 https://github.com/rockchip-linux/rkbin
 		
 #        git clone --depth 1 https://source.denx.de/u-boot/custodians/u-boot-rockchip.git -b u-boot-rockchip-20251101 u-boot
-		DDR=`ls rkbin/bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v*.bin`
-		BL31=`ls rkbin/bin/rk35/rk3588_bl31*.elf`
+		DDR=`ls rkbin/bin/rk33/rk3399_ddr_800MHz_v*.bin`
+		BL31=`ls rkbin/bin/rk33/rk3399_bl31_v*.elf`
 	export BL31=`pwd`/$BL31
 	export ROCKCHIP_TPL=`pwd`/$DDR
 echo ""
@@ -42,12 +42,13 @@ echo ""
 			exit 1
 		fi
 
-	echo 'CONFIG_SYS_SOC="rk3588"' >> configs/$1
+	echo 'CONFIG_SYS_SOC="rk3399"' >> configs/$1
 sed -i 's/#ifndef CONFIG_XPL_BUILD/#ifndef CONFIG_XPL_BUILD\n\n# define BOOT_TARGET_DEVICES_SCSI(func)	func(SCSI, scsi, 0, 0, 0) func(SCSI, scsi, 0, 0, 1)\n\n# define BOOT_TARGET_DEVICES_NVME(func)  func(NVME, nvme, 0, 0, 0) func(NVME, nvme, 0, 0, 1)\n\n/' include/configs/rockchip-common.h
 
 		make clean $1
 		make -j8
 		cp u-boot-rockchip.bin ../..
+		cp u-boot-rockchip-spi.bin ../..
 	echo "dd if=u-boot-rockchip.bin of=/dev/sdX seek=1 bs=32k conv=fsync"
 	cd ../..
 echo "DISK usage"
