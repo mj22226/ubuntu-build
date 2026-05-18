67cc39fdc0905bfdc4af73d3356b5caab5debbff
mj22226 <mj8263788@gmail.com>
2026-05-18 13:57:40 -0300

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -17,7 +17,7 @@ if [ $mem_size -gt 8 ]; then
 fi
 
 cd $linux_dir
-git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git -b linux-7.0.y
+git clone --depth 1 https://github.com/mj22226/linux.git -b linux-7.1
 
 cd linux
 make defconfig
